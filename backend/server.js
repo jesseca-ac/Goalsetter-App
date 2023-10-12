ca5f2b7c406db4d68768c612440e28e62557fbbc
jesseca-ac <jesseca.casanare@zapier.com>
2023-10-12 21:25:57 +0800

    called express, dotenv, port and app var, app listener

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('Hello world!')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const port = 5000
+const app = express()
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
+

b22d8617ffdf972246b292be43bd4f1a0d8686a8
jesseca-ac <jesseca.casanare@zapier.com>
2023-10-12 21:32:38 +0800

    .env file created, PORT created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 
-const port = 5000
+const port = process.env.PORT || 5000
 const app = express()
 
 app.listen(port, () => console.log(`Server running on port ${port}`))

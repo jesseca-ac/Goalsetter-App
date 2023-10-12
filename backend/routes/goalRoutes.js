d2c07f0be61f82889f154df495d5b3e7cffbc05e
jesseca-ac <jesseca.casanare@zapier.com>
2023-10-12 22:09:15 +0800

    crud routes created in goalRoutes.js

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,4 +5,17 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'Get Goals' })
 })
 
+router.post('/', (req, res) => {
+  res.status(200).json({ message: 'Goal Created' })
+})
+
+router.put('/:id', (req, res) => {
+  res.status(200).json({ message: `Goal ID ${req.params.id} Updated` })
+})
+
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ message: `Goal ID ${req.params.id} Deleted` })
+})
+
+
 module.exports = router

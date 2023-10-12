42a9035cbcf50df295079bdc3f62f725ee225d55
jesseca-ac <jesseca.casanare@zapier.com>
2023-10-12 21:41:18 +0800

    first route, get request

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,9 @@ const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 const app = express()
 
+app.get('/api/goals', (req, res) => {
+  res.send('Get Goals')
+})
+
 app.listen(port, () => console.log(`Server running on port ${port}`))
 

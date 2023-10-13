3c9a1357541ea72a35a912f7ed383486ba077f27
jesseca-ac <jesseca.casanare@zapier.com>
2023-10-13 23:00:22 +0800

    added express error handler, wrapped controllers in asynHandler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,35 +1,45 @@
+const asyncHandler = require('express-async-handler')
+
 
 // @route   GET /api/goals
 // @access  Private
-const getGoals = (req, res) => {
-  res.status(200).json({ message: 'Get Goals' })
-}
+const getGoals = asyncHandler(
+  async (req, res) => {
+    res.status(200).json({ message: 'Get Goals' })
+  }
+)
 
 
 // @route   POST /api/goals
 // @access  Private
-const createGoal = (req, res) => {
-  if(!req.body.text) {
-    res.status(400)
-    throw new Error('body.text is missing')
-  }
+const createGoal = asyncHandler(
+  async (req, res) => {
+    if(!req.body.text) {
+      res.status(400)
+      throw new Error('body.text is missing')
+    }
 
-  res.status(200).json({ message: 'Goal Created' })
-}
+    res.status(200).json({ message: 'Goal Created' })
+  }
+)
 
 
 // @route   PUT /api/goals
 // @access  Private
-const updateGoal = (req, res) => {
-  res.status(200).json({ message: `Goal ID ${req.params.id} Updated` })
-}
+const updateGoal = asyncHandler(
+  async (req, res) => {
+    res.status(200).json({ message: `Goal ID ${req.params.id} Updated` })
+  }
+)
 
 
 // @route   DELETE /api/goals
 // @access  Private
-const deleteGoal = (req, res) => {
-  res.status(200).json({ message: `Goal ID ${req.params.id} Deleted` })
-}
+const deleteGoal = asyncHandler(
+  async (req, res) => {
+    res.status(200).json({ message: `Goal ID ${req.params.id} Deleted` })
+  }
+)
 
 
 
